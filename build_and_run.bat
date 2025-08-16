e84a04767f175c739aab52139c186f9e6bf6ab54
Nicholas Zazzi <nznznz10@gmail.com>
2025-08-16 16:30:55 +0200

    Build refactoring completed

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,4 +1,7 @@
 @echo off
 jai component_build.jai
 jai archetype_build.jai - arch_gen "true"
-jai query_build.jai 
+jai query_build.jai
+jai observer_build.jai
+jai build.jai
+H:\PBEMJai\bin\PBEM.exe
